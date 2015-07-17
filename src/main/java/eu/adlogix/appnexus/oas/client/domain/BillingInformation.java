7dcc98b7174c82eb20ff18892ccfa765bc0010b1
Gunith Devasurendra <gunith@adlogix.eu>
2015-07-17 11:49:12 +0530

    Added modified attrs flags for BillingInfo class

--- src/main/java/eu/adlogix/appnexus/oas/client/domain/BillingInformation.java
+++ src/main/java/eu/adlogix/appnexus/oas/client/domain/BillingInformation.java
@@ -1,16 +1,25 @@
 package eu.adlogix.appnexus.oas.client.domain;
 
 import lombok.Getter;
-import lombok.Setter;
 
 /**
  * "Billing Information" which can be assigned to an {@link Advertiser} in
  * Advertiser creation, update and retrieval.
  */
 @Getter
-@Setter
 public class BillingInformation extends StatefulDomain {
 
 	private String country;
 	private String method;
+
+	public void setCountry(String country) {
+		this.country = country;
+		addModifiedAttribute("country");
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+		addModifiedAttribute("method");
+	}
+
 }
